384d00984497eda09520af11b2833cc6718c67a4
Brandon Matthews <bmatt@optimaltour.us>
2015-08-03 22:17:06 -0700

    interrupting okay and showing stack info on quit

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 
 #include <bsp.h>
+#include <inttypes.h>
 #include <pthread.h>
+#include <rtems/stackchk.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
 
 #include "hw/ecap_defn.h"
 #include "hw/pwmss_defn.h"
@@ -11,13 +12,15 @@
 #include "board_init.h"
 
 
-/* forward declarations */
+/* things that will do things later */
 rtems_task ecap_task(rtems_task_argument ignored);
 static void ecap0_handler(void* arg);
 
+/* things in which to put other things */
 rtems_id ecap_task_id;
 rtems_name ecap_task_name;
 
+/* things that do things */
 void start_tasks()
 {
     rtems_status_code ret;
@@ -72,48 +75,27 @@ rtems_task Init(rtems_task_argument arg)
 rtems_task ecap_task(rtems_task_argument arg)
 {
     struct eCAP_data ecap0_data;
-    struct PWMSS_regs* pwmss_regs = (struct PWMSS_regs*)PWMSS0_MMIO_BASE;
+    /*struct PWMSS_regs* pwmss_regs = (struct PWMSS_regs*)PWMSS0_MMIO_BASE;*/
 
     printf("ecap task starting\n");
 
     printf("initializing eCAP 0\n");
     init_ecap0(ecap0_handler, &ecap0_data);
 
-    printf("PWMSS register values = {\n"
-           "\tIDVER:\t\t0x%08"PRIx32"\n"
-           "\tSYSCONFIG:\t0x%08"PRIx32"\n"
-           "\tCLKCONFIG:\t0x%08"PRIx32"\n"
-           "\tCLKSTATUS:\t0x%08"PRIx32"\n}"
-           "\n",
-           pwmss_regs->IDVER, 
-           pwmss_regs->SYSCONFIG, 
-           pwmss_regs->CLKCONFIG, 
-           pwmss_regs->CLKSTATUS
-          );
-    printf("CONTROL_CONF_ECAP_EVT_CAP:\t0x%08"PRIu32"\n", mmio_read(SOC_CONTROL_REGS+CONTROL_CONF_ECAP_EVT_CAP));
-
-    while (1)
+    uint8_t which = 0;
+    while (ecap0_data.num_intr < 9)
     {
-        printf("ECAP0 register values ={\n"
-            "\tTSCTR:\t0x%08"PRIx32     "\tCTRPHS:\t0x%08"PRIx32"\n" 
-            "\tCAP1:\t0x%08"PRIx32      "\tCAP2:\t0x%08"PRIx32       "\tCAP3:\t0x%08"PRIx32       "\tCAP4:\t0x%08"PRIx32"\n"
-            "\tECCTL1:\t0x%04"PRIx16    "\tECCTL2:\t0x%04"PRIx16     "\tECEINT:\t0x%04"PRIx16"\n"
-            "\tECFLG:\t0x%04"PRIx16     "\tECCLR:\t0x%04"PRIx16      "\tECFRC:\t0x%04"PRIx16"\n"
-            "\tREVID:\t0x%08"PRIx32"\n"
-            "}\n",
-            ecap0_data.ecap_regs->TSCTR,  ecap0_data.ecap_regs->CTRPHS, 
-            ecap0_data.ecap_regs->CAP1,   ecap0_data.ecap_regs->CAP2,   ecap0_data.ecap_regs->CAP3,   ecap0_data.ecap_regs->CAP4,
-            ecap0_data.ecap_regs->ECCTL1, ecap0_data.ecap_regs->ECCTL2, ecap0_data.ecap_regs->ECEINT,
-            ecap0_data.ecap_regs->ECFLG,  ecap0_data.ecap_regs->ECCLR,  ecap0_data.ecap_regs->ECFRC,
-            ecap0_data.ecap_regs->REVID);
-        sleep(5);
-        printf("forcing an interrupt\n");
-        ecap0_data.ecap_regs->ECFRC = (EC_FORCE << CEVT1);
-        printf("shoulda got something\n");
+        printf("sleeping 1...");sleep(1);printf("\n");
+        printf("forcing an interrupt: CAP%1x\n", which+1);
+        ecap0_data.ecap_regs->ECFRC = (EC_FORCE << (which+1));
+        printf("sleeping 1...");sleep(1);printf("\n");
+        which = (which + 1) % 4;
         printf("ecap0_data->int_flags:\t0x%08x\n", ecap0_data.int_flags);
         printf(" that was interrupt number %lu\n", ecap0_data.num_intr);
+
     }
 
+    rtems_stack_checker_report_usage();
 }
 
 
@@ -125,16 +107,36 @@ static void ecap0_handler(void* arg)
 {
     ecap_data = (struct eCAP_data*)arg;
 
+    printk("eCAP0 got interrupt!\n");
+
+    printk("ECAP0 register values ={\n"
+        "\tTSCTR:\t0x%08"PRIx32     "\tCTRPHS:\t0x%08"PRIx32"\n" 
+        "\tCAP1:\t0x%08"PRIx32      "\tCAP2:\t0x%08"PRIx32       "\tCAP3:\t0x%08"PRIx32       "\tCAP4:\t0x%08"PRIx32"\n"
+        "\tECCTL1:\t0x%04"PRIx16    "\tECCTL2:\t0x%04"PRIx16     "\tECEINT:\t0x%04"PRIx16"\n"
+        "\tECFLG:\t0x%04"PRIx16     "\tECCLR:\t0x%04"PRIx16      "\tECFRC:\t0x%04"PRIx16"\n"
+        "\tREVID:\t0x%08"PRIx32"\n"
+        "}\n",
+        ecap_data->ecap_regs->TSCTR,  ecap_data->ecap_regs->CTRPHS, 
+        ecap_data->ecap_regs->CAP1,   ecap_data->ecap_regs->CAP2,   ecap_data->ecap_regs->CAP3,   ecap_data->ecap_regs->CAP4,
+        ecap_data->ecap_regs->ECCTL1, ecap_data->ecap_regs->ECCTL2, ecap_data->ecap_regs->ECEINT,
+        ecap_data->ecap_regs->ECFLG,  ecap_data->ecap_regs->ECCLR,  ecap_data->ecap_regs->ECFRC,
+        ecap_data->ecap_regs->REVID);
+
     ecap_data->int_flags = ecap_data->ecap_regs->ECFLG;
     ecap_data->num_intr++;
+    printk("flg, intr: 0x%04"PRIx16" 0x%04"PRIx16"\n", ecap_data->ecap_regs->ECFLG, ecap_data->num_intr);
     ecap_data->ecap_regs->ECCLR = 0xFF;
+
 }
+
+#define CONFIGURE_STACK_CHECKER_ENABLED
 
 #define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
 #define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
 
-#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
-#define CONFIGURE_MAXIMUM_TASKS 2
+#define CONFIGURE_UNIFIED_WORK_AREAS
+#define CONFIGURE_UNLIMITED_OBJECTS
 
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
 #define CONFIGURE_INIT
 #include <rtems/confdefs.h>
